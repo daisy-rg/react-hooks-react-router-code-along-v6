c6dd7ce21b37b933f9cb399c69d6f2eab6a5b694
daisy <chpdaisy@gmail.com>
2025-04-16 11:45:54 +0300

    did the navbar thingy

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,9 @@
-// About.js
+import NavBar from "../components/NavBar";
 function About() {
     return (
       <>
         <header>
-          {/* Save space for NavBar */}
+          <NavBar/>
         </header>
         <main>
           <h1>This is my about component!</h1>
